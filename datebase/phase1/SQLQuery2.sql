23788a7d23a89d06cce79c4150bbf3de97ad5b20
Andrew-bot1 <aahayes27@amarillocollege.com>
2025-05-05 02:21:48 -0500

    Update SQLQuery2.sql

--- datebase/phase1/SQLQuery2.sql
+++ datebase/phase1/SQLQuery2.sql
@@ -1,37 +1,90 @@
-create table Staff (
-	staffID int IDENTITY(1,1) PRIMARY KEY,
-	name varchar(255),
-	role varchar(255),
-	accessLevel varchar(255)
+CREATE TABLE Bills (
+    BillID int NOT NULL PRIMARY KEY,
+    OrderID int,
+    Total int
 );
+GO
 
-create table Customers (
-	customerID int IDENTITY(1,1) PRIMARY KEY,
-	name varchar(255),
-	phone varchar(255)
+CREATE TABLE Menu (
+    menuID int IDENTITY(1,1) NOT NULL primary key,
+    name varchar(255),
+    category varchar(255),
+    price float
 );
+GO
 
-create table Menu (
-	menuID int IDENTITY(1,1) PRIMARY KEY,
-	name varchar(255),
-	category varchar(255),
-	price float
+CREATE TABLE Orders (
+    OrderID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    tableID int,
+    menuID int,
+    staffID int,
+    userID int,
+    orderDate date,
+    status varchar(255)
 );
+GO
 
-create table Tables (
-	tableID int IDENTITY(1,1) PRIMARY KEY,
-	Reserved varchar(255),
-	Status varchar(255)
+CREATE TABLE Reservations (
+    ReservationId int not null PRIMARY KEY,
+    tableId int,
+    reservation_date date
 );
+GO
 
-create table Orders (
-	OrderID int IDENTITY(1,1) PRIMARY KEY,
-	tableID int FOREIGN KEY REFERENCES Tables(tableID),
-	menuID int FOREIGN KEY REFERENCES Menu(menuID),
-	staffID int FOREIGN KEY REFERENCES Staff(staffID),
-	customerID int FOREIGN KEY REFERENCES Customers(customerID),
-	orderDate date default getdate()
+CREATE TABLE Roles (
+    roleID int NOT NULL PRIMARY KEY,
+    name varchar(200) 
 );
+GO
+
+create TABLE Staff (
+    staffID int IDENTITY(1,1) not NULL PRIMARY KEY,
+    name varchar(255),
+    Salary float,
+    roleID int,
+    phone int
+);
+GO
+
+CREATE TABLE Tables (
+    tableID int IDENTITY(1,1) not NULL PRIMARY KEY,
+    Reserved varchar(255),
+    Status varchar(255)
+);
+GO
+
+CREATE TABLE Users (
+    UserID int IDENTITY(1,1) not NULL PRIMARY KEY,
+    name varchar(255),
+    phone varchar(255),
+    accessLevel varchar(255)
+);
+GO
+
+ALTER TABLE Orders ADD DEFAULT (GETDATE()) FOR orderDate;
+GO
+
+ALTER TABLE Bills ADD FOREIGN KEY (OrderID) REFERENCES Orders (OrderID);
+GO
+
+ALTER TABLE Orders ADD FOREIGN KEY (userID) REFERENCES Users (UserID);
+GO
+
+ALTER TABLE Orders ADD FOREIGN KEY (menuID) REFERENCES Menu (menuID);
+GO
+
+ALTER TABLE Orders ADD FOREIGN KEY (staffID) REFERENCES Staff (staffID);
+GO
+
+ALTER TABLE Orders ADD FOREIGN KEY (tableID) REFERENCES Tables (tableID);
+GO
+ 
+ALTER TABLE Reservations ADD FOREIGN KEY (tableId)  REFERENCES Tables (tableID);
+GO
+
+
+ALTER TABLE Staff ADD FOREIGN KEY (RoleID) REFERENCES Roles (roleID);
+GO
 
 insert into Users (name, phone, accessLevel) values ('Andrew', '547324', 'Admin');
 insert into Users (name, phone, accessLevel) values ('John', '574568', 'Manager');
